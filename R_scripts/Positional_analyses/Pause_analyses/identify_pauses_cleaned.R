e6ce97beae2603c096b5a96f025939d1c62c3660
ChiaraGiacomelli <101278133+ChiaraGiacomelli@users.noreply.github.com>
2025-08-08 18:19:49 +0200

    Add files via upload

---
+++ R_scripts/Positional_analyses/Pause_analyses/identify_pauses_cleaned.R
@@ -0,0 +1,157 @@
+# This script was developed by Dr.Pauline Herviou, Postdoctoral Scientist in the lab of Prof. Martin Bushell
+# At the CRUK Scotland Institute, Glasgow
+
+# The aim is to identify pause sites from Riboseq results
+
+#load packages----
+library(gridExtra)
+library(tidyverse)
+
+#read in common variables
+source("common_variables.R")
+
+#read in data----
+load(file = file.path(parent_dir, "counts_list.Rdata"))
+
+#import region lengths file
+region_lengths <- read_csv(file = "/FASTAs/mouse/GENCODE/vM27/transcript_info/gencode.vM27.pc_transcripts_region_lengths.csv", col_names = c("transcript", "UTR5_len", "CDS_len", "UTR3_len"))
+
+#import most abundant transcripts info
+most_abundant_transcripts <- read_csv(file = file.path(parent_dir, "most_abundant_transcripts_IDs.csv"))
+
+#data annotation function
+annotate_data <- function(df, sampl) {
+  df %>%
+    select(c("transcript", "Position", "Nucleotide", "normalised_CPM")) %>% #select needed columns
+    mutate(sample = sampl) %>%
+    inner_join(region_lengths, by = "transcript") %>%
+    mutate(region = factor(case_when(Position <= UTR5_len ~ 'utr5', #annotate transcript regions
+                                   Position <= UTR5_len + CDS_len ~ 'cds',
+                                   TRUE ~ 'utr3')),
+         cds_position = Position - UTR5_len) %>% #calculate CDS position
+  filter(region == "cds") %>% #keep only CDS data
+  select(-any_of(c("UTR5_len", "CDS_len", "UTR3_len", "region"))) -> annotated_df
+  
+  return(annotated_df)
+}  
+
+#Select CDS positions and select needed columns only for all conditions
+annotated_list <- imap(counts_list, annotate_data) #compared to lapply, imap inputs object names/indexes (here sample names as "sampl") into the function
+print(head(annotated_list[[1]]))
+
+#concatenate tables from each sample 
+all_counts <- do.call("rbind", annotated_list)
+
+#get sample info data from sample names
+all_counts %>%
+  separate_wider_delim(sample, delim="_", names=c("npm", "condition", "replicate", "seq")) %>%
+  select(-any_of(c("seq", "npm"))) -> all_mean_counts
+
+print(head(all_mean_counts))
+
+##definition of a pause from Gillen et al 2021 (https://doi.org/10.1186/s13059-021-02494-w)
+#### a pause site in each condition was defined as a position with a RPF peak height ten times greater than the average RPF peak on the mRNA####
+
+#calculate the average RPF occupancy / mRNA in cdss - control
+all_mean_counts %>%
+  filter(condition %in% c("WT", "KO")) %>%
+  distinct() %>%
+  mutate(codon = ceiling(cds_position / 3)) %>% #calculate codons
+  arrange(transcript, condition, replicate, cds_position) %>% 
+  group_by(transcript, condition, codon) %>%
+  summarise(codon_normalised_cpm = mean(normalised_CPM), codon_seq = paste(Nucleotide, collapse="")) %>% #average the normalised cpm over the 3 nt of the codons and save the codon sequences
+  ungroup() %>%
+  distinct() %>%
+  mutate(codon_seq = str_sub(codon_seq, 1, 3)) %>% #codon sequences are repeated 3 times (because we summarised 3 replicates just before) so just keep the 1st 3 letters (1st repeat)
+  pivot_wider(names_from = condition, values_from = codon_normalised_cpm, names_glue = "{condition}_{.value}") -> selected_counts #create a column for each condition
+
+print(head(selected_counts))
+print(summary(selected_counts))
+
+#calculate the average RPF peak size / mRNA in cdss - WT
+selected_counts %>%
+  filter(WT_codon_normalised_cpm > 0) %>%
+  group_by(transcript) %>%
+  summarise(WT_mean_height = mean(WT_codon_normalised_cpm)) -> wt_average_peak_height_codons
+
+#calculate the average RPF peak size / mRNA in cdss - KO
+selected_counts %>%
+  filter(KO_codon_normalised_cpm > 0) %>%
+  group_by(transcript) %>%
+  summarise(KO_mean_height = mean(KO_codon_normalised_cpm)) -> ko_average_peak_height_codons
+
+#merge with main file
+selected_counts %>%
+  left_join(wt_average_peak_height_codons, by = "transcript") %>%
+  left_join(ko_average_peak_height_codons, by = "transcript") %>%
+#identify pauses
+  mutate(WT_pause = factor(case_when(!is.na(WT_mean_height) & WT_codon_normalised_cpm >= 10*WT_mean_height ~ 'pause',
+                                         TRUE ~ 'no_pause')),
+         KO_pause = factor(case_when(!is.na(KO_mean_height) & KO_codon_normalised_cpm >= 10*KO_mean_height ~ 'pause',
+                                         TRUE ~ 'no_pause'))) -> all_mean_counts_pause_codon
+
+#print the number of pauses found
+print("NPM WT")
+length(unique(all_mean_counts_pause_codon$transcript[all_mean_counts_pause_codon$WT_pause == "pause"]))
+print("NPM KO")
+length(unique(all_mean_counts_pause_codon$transcript[all_mean_counts_pause_codon$KO_pause == "pause"]))
+
+
+all_mean_counts_pause_codon %>%
+  inner_join(most_abundant_transcripts, by = "transcript") %>%
+  filter(WT_pause == "pause" | KO_pause == "pause") -> all_mean_counts_pause_codon_filtered
+
+####b. pause sites classification:####
+#?sustained?: they are present but unaltered with treatment;
+#"resolved? in that the reduction in RPF peak height is ten times greater than the average delta decrease across the mRNA 
+#?induced? in that the increase in RPF peak height is ten times greater than the average delta increase
+
+#calculate the average change in RPFs for each transcript
+selected_counts %>%
+#calculate delta(normalised_cpm)
+  mutate(delta_cpm = KO_codon_normalised_cpm - WT_codon_normalised_cpm) %>%
+#calculate the average change upon treatment across whole mRNAs in cdss 
+  group_by(transcript) %>%
+  mutate(average_delta = mean(delta_cpm)) %>%
+  ungroup()  -> all_mean_counts_codons_delta
+
+#calculate the average increase in peak heights for up genes 
+all_mean_counts_codons_delta %>%
+  filter(average_delta > 0) %>%
+  filter(delta_cpm > 0) %>%
+  group_by(transcript) %>%
+  summarise(average_change = mean(delta_cpm)) -> increase
+
+#calculate the average decreased in peak height for down genes 
+all_mean_counts_codons_delta %>%
+  filter(average_delta < 0) %>%
+  filter(delta_cpm < 0) %>%
+  group_by(transcript) %>%
+  summarise(average_change = mean(delta_cpm)) -> decrease
+
+#concatenate and merge with delta data
+all_mean_counts_codons_delta %>%
+  inner_join(most_abundant_transcripts, by = "transcript") %>%
+  inner_join(rbind(increase, decrease), by = "transcript") %>%
+  inner_join(all_mean_counts_pause_codon_filtered %>% select(all_of(c("transcript", "codon", "WT_pause", "KO_pause"))), by = c("transcript", "codon")) %>%
+#define induced, resolved and sustained pauses
+  mutate(pause_type = factor(case_when(average_delta > 0 & delta_cpm > 10*average_change ~ "induced", 
+                                       average_delta < 0 & delta_cpm > -10*average_change ~ "induced", 
+                                       average_delta < 0 & delta_cpm < 10*average_change ~ "resolved", 
+                                       average_delta > 0 & delta_cpm < - 10*average_change ~ "resolved", 
+                                       WT_pause == "pause" & KO_pause == "pause" ~ "sustained",
+                                       TRUE ~ "ns")))  -> pause_type
+
+#extract the sequence of the codons in the E and and A sites
+pause_type %>%
+  rename(p_site = codon, p_site_seq = codon_seq) %>%
+  mutate(e_site = p_site - 1, 
+         a_site = p_site + 1) %>%
+  left_join(selected_counts %>% rename(e_site = codon, e_site_seq = codon_seq) %>% select(c("transcript", "e_site", "e_site_seq")), by = c("transcript", "e_site")) %>%
+  left_join(selected_counts %>% rename(a_site = codon, a_site_seq = codon_seq) %>% select(c("transcript", "a_site", "a_site_seq")), by = c("transcript", "a_site")) %>%
+  select(-c("e_site", "a_site")) -> pause_type
+
+#export data
+write_csv(pause_type, file.path(parent_dir, "NPM_pause_analysis.csv"))
+
+
---
+++ R_scripts/Positional_analyses/Pause_analyses/make_plots_wLEgroups_cleaned.R
@@ -0,0 +1,204 @@
+# This script was originally developed by Dr.Pauline Herviou, Postdoctoral Scientist in the lab of Prof. Martin Bushell
+# At the CRUK Scotland Institute, Glasgow
+
+# The plot function from her script was then adapted to suit the leading edge data from the Gene Set Enrichment Analyses
+
+# load libraries & set conflicts ------
+library(tidyverse)
+library(scales)
+library(gridExtra)
+library(grid)
+library(fgsea)
+library(conflicted)
+
+conflicts_prefer(dplyr::filter)
+conflicts_prefer(dplyr::select)
+conflicts_prefer(purrr::reduce)
+
+# choose your Machine
+#VM
+#machine_dir <- '~/data'
+#Rstudio on Windows at work
+#machine_dir <- 'N:'
+# VPN connected on Mac
+machine_dir <- '/Volumes/data-1'
+
+control <- "WT"
+treatment <- "KO"
+
+Tissue <- "APC-KRAS"
+
+#read in common variables
+source(paste0(machine_dir,"/CGIACOME/AAJ_NPM/20230622_RiboSeq_APCKRAS/Ribo-seq-Ribo-seq2.0/R_scripts/common_variables.R"))
+
+#read in functions----
+source(paste0(machine_dir,"/CGIACOME/AAJ_NPM/20230622_RiboSeq_APCKRAS/Ribo-seq-Ribo-seq2.0/R_scripts/binning_RiboSeq_functions.R"))
+
+plot_dir <- paste0(parent_dir, "/plots/pauses/")
+
+#import pause tables
+pauses <- read_csv(paste0(machine_dir,"/CGIACOME/AAJ_NPM/20250117_data_Pauline/NPM_pause_analysis_20250331.csv"))
+
+#import most abundant transcripts info
+most_abundant_transcripts <- read_csv(file = paste0(machine_dir,"/CGIACOME/AAJ_NPM/20230622_RiboSeq_APCKRAS/Ribo-seq-Ribo-seq2.0/Analysis/most_abundant_transcripts/most_abundant_transcripts_IDs.csv"))
+
+#import region lengths table
+region_lengths <- read_csv(file = paste0(machine_dir, "/R11/bioinformatics_resources/FASTAs/mouse/GENCODE/vM27/transcript_info/gencode.vM27.pc_transcripts_region_lengths.csv"),
+                                         col_names = c("transcript", "UTR5_len", "CDS_len", "UTR3_len"))
+
+codon_AA <- read_csv(file = file.path(machine_dir,"CGIACOME/AAJ_NPM/20250117_data_Pauline/bioinformatics_resources_codons/codon_amino_acid_pairs_and_properties.csv"))
+codon_anticod <- read_csv(file = file.path(machine_dir,"CGIACOME/AAJ_NPM/20250117_data_Pauline/bioinformatics_resources_codons/codon_anticodon_pairs.csv"))
+
+# Format table to have all info -----
+pauses %>%
+  mutate(across(c(e_site_seq, p_site_seq, a_site_seq), ~as.character(gsub("T", "U", .)))) %>% # make into "RNA" codons with U instead of T
+  left_join(codon_AA, by = join_by(p_site_seq == codon)) %>% 
+  left_join(codon_AA, by = join_by(a_site_seq == codon), suffix = c("_P", "_A")) %>% 
+  left_join(codon_AA, by = join_by(e_site_seq == codon)) %>% 
+  rename(AA_E = AA,
+         Symbol_E = Symbol,
+         Properties_E = Properties) %>% 
+  mutate(
+    E3 = str_sub(e_site_seq, -1),  # Extract last character of e_site_seq
+    P3 = str_sub(p_site_seq, -1),  # Extract last character of p_site_seq
+    A3 = str_sub(a_site_seq, -1)   # Extract last character of a_site_seq
+  ) %>% 
+  filter(!is.na(e_site_seq) & !is.na(a_site_seq) & !is.na(AA_A)) %>% # removes pause sites at the start codon and stop codon
+  filter(pause_type != "ns",
+         p_site !=1) %>% 
+  mutate(pause_type = as_factor(pause_type)) %>%
+  mutate(pause_type = fct_relevel(pause_type, c("induced", "resolved", "sustained"))) %>% 
+  relocate(WT_pause, KO_pause, pause_type, .after = gene_sym) %>% 
+  mutate(across(c(8:22), ~as.factor(.))) -> PH_AA
+
+pauses %>% 
+  filter(pause_type != "ns",
+         p_site !=1) -> pauses
+
+plot_pauses_groups <- function(df, pathway) {
+  
+  # get info out from the leading edge merged data
+  LE %>% filter(pathway == !!pathway) -> POI_row
+  
+  prot_genes <- if(nchar(POI_row$leadingEdge_Prot) > 0) {
+    str_split(POI_row$leadingEdge_Prot, "\\s+")[[1]]
+  } else {
+    character(0)
+  }
+  
+  rpf_genes <- if(nchar(POI_row$leadingEdge_RPFs) > 0) {
+    str_split(POI_row$leadingEdge_RPFs, "\\s+")[[1]]
+  } else {
+    character(0)
+  }
+  
+  common_genes <- intersect(prot_genes, rpf_genes)
+  only_prot <- setdiff(prot_genes, rpf_genes)
+  only_rpf <- setdiff(rpf_genes, prot_genes)
+  
+  POI_row %>% pull(NES_Prot) -> N_P
+  POI_row %>% pull(NES_RPFs) -> N_R
+  POI_row %>% pull(padj_Prot) -> padj_P
+  POI_row %>% pull(padj_RPFs) -> padj_R
+  
+  POI_row %>% pull(count) -> GIP # genes in pathway
+  POI_row %>% pull(size_RPFs) -> DR # detected ion riboseq
+  POI_row %>% pull(size_Prot) -> DP # detected in proteomics
+  
+  df %>%
+    mutate(gene_group = case_when(gene_sym %in% common_genes ~ "Shared",
+                                  gene_sym %in% only_prot ~ "Only Protein",
+                                  gene_sym %in% only_rpf ~ "Only RPFs",
+                                  TRUE ~ "none"),
+           alpha_score = ifelse(gene_group %in% c("Shared", "Only Protein", "Only RPFs"), 1, 0.01)) %>%
+    mutate(gene_group = factor(gene_group, levels = c("Shared", "Only Protein", "Only RPFs", "none"))) %>%
+    arrange(desc(gene_group)) -> df
+  
+  df %>% 
+    filter(gene_group != "none") %>% 
+    distinct(gene_sym, gene_group) %>% 
+    count(gene_group) -> group_counts
+  
+  setNames(paste0(group_counts$gene_group, " (n=", group_counts$n, ")"),
+           group_counts$gene_group) -> labeller_vec
+  
+  df %>% 
+    filter(gene_group != "none") %>% 
+    inner_join(region_lengths[,c("transcript", "CDS_len")], by = "transcript") %>% #merge with CDS lengths data
+    mutate(relative_position = (p_site + 1)/(CDS_len/3)) %>%  #length normalise (CDS / 3 to convert nt to codons)
+    ggplot(aes(x=relative_position, fill=pause_type)) + 
+    geom_density(alpha = 0.7) +
+    facet_wrap(~ gene_group, scales = "fixed", labeller = as_labeller(labeller_vec)) +
+    scale_fill_manual(values = c("induced" = "red3", "sustained" = "grey40", "resolved" = "royalblue")) +
+    theme_classic() +
+    theme(aspect.ratio = 1) +
+    labs(
+      x = "% CDS",
+      y = "Pause density",
+      title = paste(paste(Tissue, "Npm1 KO"), str_replace_all(pathway, "_", " "), paste("Genes in pathway:", GIP), sep = "\n"),
+      subtitle = paste("NES Protein", round(N_P, 2),
+                       "\nadj pvalue Protein", signif(padj_P, 2),
+                       "\nDetected Proteins:", DP,
+                       "\nNES RPFs", round(N_R, 2),
+                       "\nadj pvalue RPFs", signif(padj_R, 2),
+                       "\nDetected in Riboseq:", DR)) +
+    theme_bw() +
+    theme(axis.title = element_text(size = 14),
+          strip.text = element_text(size = 14),
+          axis.text = element_text(size = 10)) -> density_plot
+  
+  pdf(file = paste0(plot_dir, paste(pathway, "RPF_Prot_pause_density_plot_fixedAx_allProp.pdf", sep = "_")), width = 10, height = 7)
+  print(density_plot)
+  dev.off()
+  
+  return(density_plot)
+}
+
+# On gene groups from the leading edges of gseas ---------
+# for every pathway, do the plot of the pause distribution across CDSs by pause type
+
+# fetch number of genes in each pathway - regardless of whether they are detected or not
+source(paste0(machine_dir,"/CGIACOME/AAJ_NPM/20230622_RiboSeq_APCKRAS/Ribo-seq-Ribo-seq2.0/R_scripts/read_mouse_GSEA_pathways.R"))
+
+## read in leading edges HALLMARKS ---------
+LE_RPFs <- read_tsv(file = file.path(parent_dir, "plots/fgsea/scatters/AK_Npm1KO_Hallmark_RPF_BC_LeadEdge.tsv")) 
+LE_Protein <- read_tsv(file = file.path(parent_dir, "plots/fgsea/Proteomics/scatters/AK_Npm1KO_Hallmark_LeadEdge.tsv"))
+
+HM <- data.frame(pathway = names(pathways.hallmark),
+                 count = sapply(pathways.hallmark, length),
+                 stringsAsFactors = FALSE)
+HM %>% remove_rownames() -> HM # number of genes in each pathway
+
+# as of 20250325 - this analysis done on all shared pathways and not only on the significant ones
+LE_Protein %>% 
+  left_join(LE_RPFs, by = join_by(pathway), suffix = c("_Prot", "_RPFs")) %>% 
+  left_join(HM)-> LE
+
+LE %>% pull(pathway) -> POI
+
+# plot
+lapply(POI, plot_pauses_groups, df = pauses)
+
+## read in leading edges GOBP ---------
+LE_RPFs <- read_tsv(file = file.path(parent_dir, "plots/fgsea/scatters/AK_Npm1KO_GOBP_RPF_BC_LeadEdge.tsv")) 
+LE_Protein <- read_tsv(file = file.path(parent_dir, "plots/fgsea/Proteomics/scatters/AK_Npm1KO_BP_LeadEdge.tsv"))
+
+GOBP <- data.frame(pathway = names(pathways.bio_processes),
+                   count = sapply(pathways.bio_processes, length),
+                   stringsAsFactors = FALSE)
+GOBP %>% remove_rownames() -> GOBP
+
+# as of 20250325 - this analysis done on all shared pathways and not only on the significant ones
+LE_Protein %>% 
+  left_join(LE_RPFs, by = join_by(pathway), suffix = c("_Prot", "_RPFs")) %>% 
+  left_join(GOBP) -> LE
+
+# for GOBP there are so many pathways that it makes no sense to plot all
+# plotting is done only for the two pathways of interest, which are ER stress and ISR
+LE %>%
+  filter(pathway == "GOBP_RESPONSE_TO_ENDOPLASMIC_RETICULUM_STRESS" | 
+           pathway == "GOBP_INTEGRATED_STRESS_RESPONSE_SIGNALING") %>% 
+  pull(pathway) -> POI
+
+# plot
+lapply(POI, plot_pauses_groups, df = pauses)
