a35af675ccf98a1a00547fdd8bf4585196b92d3c
Chiara Giacomelli <101278133+ChiaraGiacomelli@users.noreply.github.com>
2025-10-28 12:08:37 +0100

    Update read_mouse_GSEA_pathways.R
    Generalised paths

--- R_scripts/Functional_analyses/read_mouse_GSEA_pathways.R
+++ R_scripts/Functional_analyses/read_mouse_GSEA_pathways.R
@@ -1,16 +1,13 @@
-# choose your Machine in the common variables!!!
 # Please note the version of signatures used: This was the first release of MSigDB in 2023
+# read from the folder where you have saved the gmt files downloaed from MSigDB
 
-pathways.hallmark <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/mh.all.v2023.1.Mm.symbols.gmt"))
-pathways.curated <- gmtPathways(paste0(machine_dir,"/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.all.v2023.1.Mm.symbols.gmt"))
-pathways.miRNA_targets <- gmtPathways(paste0(machine_dir, "/R11/bioinformatics_resources/GSEA/m3.mirdb.v2022.1.Mm.symbols.gmt"))
-pathways.transcription_factors <- gmtPathways(paste0(machine_dir, "/R11/bioinformatics_resources/GSEA/m3.gtrd.v2022.1.Mm.symbols.gmt"))
-pathways.mol_funs <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.mf.v2023.1.Mm.symbols.gmt"))
-pathways.bio_processes <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.bp.v2023.1.Mm.symbols.gmt"))
-pathways.cell_comp <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.cc.v2023.1.Mm.symbols.gmt"))
-pathways.tumour_phen_onto <- gmtPathways(paste0(machine_dir, "/R11/bioinformatics_resources/GSEA/m5.mpt.v2022.1.Mm.symbols.gmt"))
-pathways.cell_type_sig <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m8.all.v2023.1.Mm.symbols.gmt"))
-pathways.biocarta <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.biocarta.v2023.1.Mm.symbols.gmt"))
-pathways.reactome <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.reactome.v2023.1.Mm.symbols.gmt"))
-pathways.wp <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.wikipathways.v2023.1.Mm.symbols.gmt"))
-pathways.custom <- gmtPathways(paste0(machine_dir, "/CGIACOME/Protocols_and_Documentation/Sequencing/Gene_signatures/msigdb_v2023.1.Mm_GMTs/20230411_custom_MMu_signatures.gmt"))
+pathways.hallmark <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/mh.all.v2023.1.Mm.symbols.gmt")
+pathways.curated <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.all.v2023.1.Mm.symbols.gmt"))
+pathways.mol_funs <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.mf.v2023.1.Mm.symbols.gmt"))
+pathways.bio_processes <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.bp.v2023.1.Mm.symbols.gmt"))
+pathways.cell_comp <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m5.go.cc.v2023.1.Mm.symbols.gmt"))
+pathways.cell_type_sig <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m8.all.v2023.1.Mm.symbols.gmt"))
+pathways.biocarta <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.biocarta.v2023.1.Mm.symbols.gmt"))
+pathways.reactome <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.reactome.v2023.1.Mm.symbols.gmt"))
+pathways.wp <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/m2.cp.wikipathways.v2023.1.Mm.symbols.gmt"))
+pathways.custom <- gmtPathways("Gene_signatures/msigdb_v2023.1.Mm_GMTs/20230411_custom_MMu_signatures.gmt"))
